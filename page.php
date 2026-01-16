1036a9c045dcb781c542f6fe9758ceb82559e978
cdsanto <47996383+cdsanto@users.noreply.github.com>
2026-01-15 20:26:35 -0600

    Add files via upload

---
+++ page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UNIR</title>
+</head>
+<body>
+    <h1>Maestría en DevOps</h1>
+    <?php
+    	echo "<h2>Actividad 2.</h2>";
+    	echo "<p>Equipo 1058A.</p>";
+    ?>
+        <ul>
+    		<li>Christian</li>
+    		<li>Joel</li>
+    		<li>Osvaldo</li>
+    		<li>Pablo</li>
+		</ul>
+	<?php
+        echo "<p>Hola desde PHP, hoy es " . date("d/m/Y") . ".</p>";
+    ?>
+</body>
+</html>
+
